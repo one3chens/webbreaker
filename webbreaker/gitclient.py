98c31d507a57d7ac482a458c7b58543855602a59
Matthew.Dunaj <z002r0t@dca9046e8d47.hq.target.com>
2017-09-20 14:51:11 -0500

    Refactor gitapi structure

--- webbreaker/gitclient.py
+++ webbreaker/gitclient.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from webbreaker.gitapi import GitApi
+from gitapi.git import GitApi
 from webbreaker.webbreakerlogger import Logger
 import requests
 import requests.exceptions
